db83ae514508c2bf6c27969e8213bfd2c1c52f78
Kalpesh <kalpeshlokhande0355@gmail.com>
2025-07-01 22:22:36 +0530

    search feature

--- netflix_site/core/urls.py
+++ netflix_site/core/urls.py
@@ -10,5 +10,6 @@ urlpatterns=[
     path('movie/<str:pk>/',views.Movie, name='movie'),
     path('my-list',views.my_list, name='my-list'),
     path('add-to-list',views.add_to_list ,name='add-to-list'),
+    path('search',views.search,name='search'),
 
 ]
